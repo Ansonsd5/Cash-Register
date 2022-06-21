e16935bca7d7dc6f0e6e95cbba4e6ef69c72a4bc
Ansonsd5 <anson11sd@gmail.com>
2022-06-21 23:12:50 +0530

    good to go!
    Done with complete app

--- logic.js
+++ logic.js
@@ -1,28 +1,43 @@
 const billAmount = document.querySelector("#billAmount");
-
 const cashGiven = document.querySelector("#cashGiven");
-
 const checkBtn = document.querySelector(".btnCheck");
 const output = document.querySelector("#output");
-
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+const message = document.querySelector("#error-message")
+const notesReturned = document.querySelectorAll(".notesTobereturned");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 function ValidateBillandCashAmount() {
+   HideMessage()
    if (billAmount.value > 0) {
-      if (billAmount.value < cashGiven.value) {
-         console.log("logic")
-      } 
-      else {
-         console.log("Cash given is Insufficent");
+      if (cashGiven.value >= billAmount.value) {
+         const amountToBeReturned = cashGiven.value - billAmount.value;
+         calculateChange(amountToBeReturned)
+      } else {
+         ShowMessage("The Cash given is Insuficent")
       }
 
- }
- else{
-   console.log("Enter a valid Input")
- }
- }
+   } else {
+    ShowMessage("Enter a Valid bill amount")
+   }
+}
+function calculateChange(amountToBeReturned) {
+   for (let i = 0; i < availableNotes.length; i++) {
+      const notesToBeRetrun = Math.trunc(amountToBeReturned / availableNotes[i]);
+      amountToBeReturned = amountToBeReturned % availableNotes[i];
+     
+      notesReturned[i].innerText= notesToBeRetrun;
+      console.log(notesToBeRetrun)
+   }
+}
 
+function ShowMessage(str){
+   message.style.display ="block";
+      message.innerText= str;
+   
+}
 
-
+function HideMessage(){
+   message.style.display ="none";    
+}
 
 checkBtn.addEventListener("click", ValidateBillandCashAmount)
