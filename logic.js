ec98168188626bf1b9fe8a590a324b7d8b58338d
Ansonsd5 <anson11sd@gmail.com>
2022-06-21 16:54:48 +0530

    initial commit

--- logic.js
+++ logic.js
@@ -1,14 +1,28 @@
-const billAmount = document.querySelector(".billAmount");
+const billAmount = document.querySelector("#billAmount");
 
-const cashGiven = document.querySelector(".cashGiven");
+const cashGiven = document.querySelector("#cashGiven");
 
 const checkBtn = document.querySelector(".btnCheck");
+const output = document.querySelector("#output");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+
+function ValidateBillandCashAmount() {
+   if (billAmount.value > 0) {
+      if (billAmount.value < cashGiven.value) {
+         console.log("logic")
+      } 
+      else {
+         console.log("Cash given is Insufficent");
+      }
+
+ }
+ else{
+   console.log("Enter a valid Input")
+ }
+ }
 
 
-function BillandCashAmount(){
-   console.log(billAmount.value);
-   console.log(cashGiven.value)
-}
 
-checkBtn.addEventListener("click", BillandCashAmount)
 
+checkBtn.addEventListener("click", ValidateBillandCashAmount)
